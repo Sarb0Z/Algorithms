e94b2a902bb56dca1cbba93bd3ead881139a7f12
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-01-27 20:09:04 +0500

    logic explained, implementation left

--- 6.Longest_Palindromic_Substring.py
+++ 6.Longest_Palindromic_Substring.py
@@ -1,5 +1,17 @@
 
-#Given a string s, return the longest palindromic substring in s.
-
+# Given a string s, return the longest palindromic substring in s.
+"""Algorithm"""
+    # at each point of the string, we will simultaneously go forward and back
+    # until we reach the end or beginning of string or the characters at both 
+    # indexes are not the same
+    # if at this point, the radius is greater than previous, we will replace
+    # the previous substring with this one
+    # pivot will center on character for odd palindrome and between characters
+    # for even palindrome 
+        
 def longestPalindrome(self, s: str) -> str:
-    
+    if len(s) < 2:
+        return s
+    pivot=0
+    radius=0
+    subStr=""
