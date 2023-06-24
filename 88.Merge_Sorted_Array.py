b39df366a0431a2ccc09ad19a28564a48c538f13
Sarb0z <sarb0z@mail.com>
2023-06-24 16:04:00 -0400

    incorrect sol

---
+++ 88.Merge_Sorted_Array.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i=0
+        j=0
+        temp=0
+        while (i<(m+n) and j<n):
+            print(nums1)
+…                i+=1
